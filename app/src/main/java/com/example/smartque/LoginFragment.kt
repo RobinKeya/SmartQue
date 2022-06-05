d0d5add17efb596bf9ca5ad98e7f1919e6281fac
RobinKeya <maimakrobin@gmail.com>
2022-06-05 18:23:03 +0300

    First commit, majorly UIs done

---
+++ app/src/main/java/com/example/smartque/LoginFragment.kt
@@ -0,0 +1,54 @@
+package com.example.smartque
+
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.example.smartque.databinding.FragmentLoginBinding
+
+import com.example.smartque.R
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //val usernameEditText = binding.username
+        //val passwordEditText = binding.password
+        //val loginButton = binding.login
+        //val loadingProgressBar = binding.loading
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
